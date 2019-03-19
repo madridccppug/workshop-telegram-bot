27d7e153a20892bcefa8f6bae9debba4a23f2efe
Javier G. Sogo <jgsogo@gmail.com>
2019-03-19 14:03:50 +0100

    wordnet game

---
+++ wordnet_game/main.cpp
@@ -0,0 +1,31 @@
+
+#include <iostream>
+#include <sstream>
+#include <stdio.h>
+
+#include <wnb/core/wordnet.hh>
+
+#include "bot.h"
+
+
+int main() {
+
+    const char* token = std::getenv("MADRIDCCPPUG_BOT_TOKEN");
+    const char* wnet_data = std::getenv("WORDNET_DATA_PATH");
+    if (!token or !wnet_data) {
+        std::cerr << "Provide env variables MADRIDCCPPUG_BOT_TOKEN and WORDNET_DATA_PATH\n";
+        return 1;
+    }
+
+    wnb::wordnet wn(wnet_data);
+    Bot bot(token, wn);
+
+    try {
+        bot.run();
+    }
+    catch (TgBot::TgException& e) {
+        printf("error: %s\n", e.what());
+    }
+
+    return 0;
+}
